a4216ed0b21d7e52bc8b9b67a0ede8c2ddc03744
June-Young Ahn <jyoungahn@gmail.com>
2020-10-28 15:19:47 +0900

    Dummy ocr models added.

---
+++ lib/ocr_models/google_tesseract_ocr.dart
@@ -0,0 +1,5 @@
+class TesseractOcr {
+  String readText(String imagePath) {
+    return '123가5678 (Tesseract 연동 前)';
+  }
+}
--- lib/ocr_models/sfmi_da_ocr_tensorflowlite.dart
+++ lib/ocr_models/sfmi_da_ocr_tensorflowlite.dart
@@ -1,1 +1,5 @@
-//
+class SfmiDaOcrMobile {
+  String readText(String imagePath) {
+    return '123가5678 (TFLite 연동 前)';
+  }
+}
