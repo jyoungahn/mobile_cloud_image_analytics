b8859f5739faece1a88cbb24dcb0f6fa3b43894e
June-Young Ahn <jyoungahn@gmail.com>
2020-11-02 10:35:31 +0900

    Update api_keys.dart

--- lib/security/api_keys.dart
+++ lib/security/api_keys.dart
@@ -1,5 +1,6 @@
 // Microsoft Azure Cognitive Services API Key
-final String AZURE_API_KEY = '45848a51ef554984a9d4a5104d4c58ac';
+final String AZURE_API_KEY = '*** type your api key ***';
+
 
 // Google Cloud Platform API Key
-final String GCP_API_KEY = 'AIzaSyCg7WDQXrduxROWtmQE3rTVOLdPXLUdL74';
+final String GCP_API_KEY = '*** type your api key ***';
