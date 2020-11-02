f544e8bf751b252df8b2db2592a4588a0c4aa8f9
June-Young Ahn <jyoungahn@gmail.com>
2020-11-02 10:33:32 +0900

    Azure API Key and Google API Key sample.

--- lib/security/api_keys.dart
+++ lib/security/api_keys.dart
@@ -1,5 +1,5 @@
 // Microsoft Azure Cognitive Services API Key
-final String AZURE_API_KEY = '**************************************';
+final String AZURE_API_KEY = '45848a51ef554984a9d4a5104d4c58ac';
 
 // Google Cloud Platform API Key
-final String GCP_API_KEY = '***************************************';
+final String GCP_API_KEY = 'AIzaSyCg7WDQXrduxROWtmQE3rTVOLdPXLUdL74';
